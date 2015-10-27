52a3e02d64be892b795ce28dee5b4822a0ba6a2a
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-27 14:14:15 -0700

    all pages updated to use Sinatra

--- my-site.rb
+++ my-site.rb
@@ -5,4 +5,16 @@ class MySite < Sinatra::Base
   get "/" do
     erb :index
   end
+
+  get "/about" do
+    erb :about
+  end
+
+  get "/projects" do
+    erb :projects
+  end
+
+  get "/blog" do
+    erb :blog
+  end
 end
