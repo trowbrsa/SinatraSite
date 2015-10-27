d7f08ffeb527d369ce0db3a56e714e492e0a053b
Daphne <dg1181@nyu.edu>
2015-10-27 15:50:56 -0700

    Updated cat burrito to be super bouncy

--- my-site.rb
+++ my-site.rb
@@ -4,4 +4,16 @@ class MySite < Sinatra::Base
   get "/" do
     erb :index
   end
+  get "/index.html" do
+    erb :index
+  end
+  get "/about.html" do
+    erb :about
+  end
+  get "/blog.html" do
+    erb :blog
+  end
+  get "/projects.html" do
+    erb :projects
+  end
 end
