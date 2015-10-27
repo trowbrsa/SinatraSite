e14a85c77c3eaadf607cc2ed22d33b311f98d5b9
Daphne <dg1181@nyu.edu>
2015-10-27 16:56:52 -0700

    Added some white space between verb/route combos in my-site.rb

--- my-site.rb
+++ my-site.rb
@@ -4,15 +4,19 @@ class MySite < Sinatra::Base
   get "/" do
     erb :index
   end
+
   get "/index.html" do
     erb :index
   end
+
   get "/about.html" do
     erb :about
   end
+
   get "/blog.html" do
     erb :blog
   end
+
   get "/projects.html" do
     erb :projects
   end
