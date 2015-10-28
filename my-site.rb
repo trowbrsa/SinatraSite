b7cd5bd6923aabc52f54356eaf524661252aaf8b
Sarah <trowbrsa@gmail.com>
2015-10-28 09:37:50 -0700

    Added more space between blocks in my-site.rb

--- my-site.rb
+++ my-site.rb
@@ -2,13 +2,21 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
+
+  get "/" do
+    erb :index
+  end
+
   get "/index" do
     erb :index
   end
+
   get "/projects" do
     erb :projects
   end
+
   get "/blog" do
     erb :blog
   end
+  
 end
