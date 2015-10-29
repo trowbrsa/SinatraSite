e9737a8a95cbcbc7e414fa4f35d976146182c54c
Sarah <trowbrsa@gmail.com>
2015-10-29 16:34:55 -0700

    Added time.now method

--- my-site.rb
+++ my-site.rb
@@ -4,18 +4,26 @@ class MySite < Sinatra::Base
 
 
   get "/" do
+    @title = 'My Homepage'
     erb :index
   end
 
+
   get "/index" do
+    @title = 'My Homepage'
+    @h1 = "Sarah Trowbridge"
     erb :index
   end
 
   get "/projects" do
+    @title = "Sarah's projects"
+    @h1 = "Projects"
     erb :projects
   end
 
   get "/blog" do
+    @title = "Blog"
+    @h1 = "Blog"
     erb :blog
   end
 
